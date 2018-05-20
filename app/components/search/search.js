3c3256d3a86e4841d8f6b86f2f8b077748ef319a
Hassan Sani <saniyhassan@gmail.com>
2018-05-20 22:54:32 +0100

    Switching the repo to public repo, previously PRIVATE

--- app/components/search/search.js
+++ app/components/search/search.js
@@ -1,10 +1,5 @@
 angular.module('MusicSearch.search', ['ngRoute', 'ngAnimate', 'ngTouch', 'spotify'])
 .config(['$routeProvider', '$locationProvider', 'SpotifyProvider', function($routeProvider, $locationProvider, SpotifyProvider) {
-	SpotifyProvider.setClientId('88b57db6bfe6437a97b58a6e5450ca05');
-	SpotifyProvider.setRedirectUri('http://localhost:8888/#!/callback');
-	SpotifyProvider.setScope('user-read-private playlist-read-private playlist-modify-private playlist-modify-public');
-	// If you already have an auth token
-	SpotifyProvider.setAuthToken('zoasliu1248sdfuiknuha7882iu4rnuwehifskmkiuwhjg23');
     $routeProvider.when('/search', {
 		 templateUrl: 'components/search/search.html',
 		 controller: 'SearchCtrl'
@@ -23,9 +18,7 @@ angular.module('MusicSearch.search', ['ngRoute', 'ngAnimate', 'ngTouch', 'spotif
 	prams.join(' ');
 
 	$scope.getResult = ''
-	$scope.$watch(function () { return CommonProp.GetData(); }, function (newValue, oldValue) {
-		console.log(newValue, oldValue);
-		
+	$scope.$watch(function () { return CommonProp.GetData(); }, function (newValue, oldValue) {		
         if (newValue !== oldValue){
 			$scope.getResult = newValue;
 		}
